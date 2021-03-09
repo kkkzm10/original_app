23b534b7bae999ab092fb425cd5c330d5f8c264d
kkkzm10 <78135780+kkkzm10@users.noreply.github.com>
2021-03-09 11:31:56 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :mates, only:[:index, :show]
   resources :messages, only: [:index]
   resources :users, only: [:index, :show, :edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end  
 end
