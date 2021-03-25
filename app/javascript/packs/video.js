f6646a5e1a89ffab0ec1bda593f71d6ead2e81c3
kkkzm10 <78135780+kkkzm10@users.noreply.github.com>
2021-03-25 10:44:15 +0900

    Herokuでアセットパイプラインを通るようにするための修正

--- app/javascript/packs/video.js
+++ app/javascript/packs/video.js
@@ -1,38 +1,38 @@
-  //Peer作成
-  const peer = new Peer({
-    key: 'e1d3f48b-663c-46c9-8918-478d100b03e5',
-    debug: 3
-  });
+//Peer作成
+const peer = new Peer({
+  key: 'e1d3f48b-663c-46c9-8918-478d100b03e5',
+  debug: 3
+});
 
-  //PeerID取得
+//PeerID取得
 peer.on('open', () => {
-  document.getElementById('my-id').textContent = peer.id;
+document.getElementById('my-id').textContent = peer.id;
 });
 
 // 発信処理
 document.getElementById('make-call').onclick = () => {
-  const theirID = document.getElementById('their-id').value;
-  const mediaConnection = peer.call(theirID, localStream);
-  setEventListener(mediaConnection);
+const theirID = document.getElementById('their-id').value;
+const mediaConnection = peer.call(theirID, localStream);
+setEventListener(mediaConnection);
 };
 
 // イベントリスナを設置する関数
 const setEventListener = mediaConnection => {
-  mediaConnection.on('stream', stream => {
-    // video要素にカメラ映像をセットして再生
-    const videoElm = document.getElementById('their-video')
-    videoElm.srcObject = stream;
-    videoElm.play();
-  });
+mediaConnection.on('stream', stream => {
+  // video要素にカメラ映像をセットして再生
+  const videoElm = document.getElementById('their-video')
+  videoElm.srcObject = stream;
+  videoElm.play();
+});
 }
 
 //着信処理
 peer.on('call', mediaConnection => {
-  mediaConnection.answer(localStream);
-  setEventListener(mediaConnection);
+mediaConnection.answer(localStream);
+setEventListener(mediaConnection);
 });
 
 //通信切断
 peer.on('close', () => {
-  alert('通信が切断しました。');
+alert('通信が切断しました。');
 });
