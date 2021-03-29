c105418565d36ffe9d7e51200b5c6ac10c2bd61a
kkkzm10 <78135780+kkkzm10@users.noreply.github.com>
2021-03-29 11:23:38 +0900

    usersテーブルでのimageカラムの重複を解消

--- db/schema.rb
+++ db/schema.rb
@@ -74,12 +74,12 @@ ActiveRecord::Schema.define(version: 2021_03_09_100345) do
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.text "self_introduction", null: false
-    t.string "image", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
